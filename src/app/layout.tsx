c5ec5687167314384f551478932fbb52c5910b93
Aleksey Petrenko <>
2024-10-10 01:10:20 +0200

    WAU-0001 deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,85 @@
-// app/layout.tsx
+import { Box, HStack, Icon, Heading, Text } from "@chakra-ui/react";
+import { ReactNode } from "react";
+import NextLink from "next/link";
+import { FaHome, FaStore, FaInfoCircle } from "react-icons/fa";
 
-import { ChakraProvider } from '@chakra-ui/react';
-import './globals.css'; // Import any global CSS styles you have
-import type { ReactNode } from 'react';
-
-export const metadata = {
-  title: 'My Next.js App',
-  description: 'A Next.js app with Chakra UI',
-};
-
-export default function RootLayout({ children }: { children: ReactNode }) {
+function Menu() {
   return (
-    <html lang="en">
-      <body>
-        <ChakraProvider>
-          {children}
-        </ChakraProvider>
-      </body>
-    </html>
+    <Box
+      as="footer"
+      position="fixed"
+      bottom={10}
+      left={10}
+      right={10}
+      borderWidth="2px"
+      borderRadius="16px"
+      borderColor={"red"}
+      backgroundColor={"rgba(0,0,0,0.1)"}
+      p={8}
+    >
+      <HStack justifyContent="space-around">
+        <NextLink href="/" passHref>
+          <HStack>
+            <Icon as={FaHome} boxSize={48}  color={'#002c8b'}/>
+          </HStack>
+        </NextLink>
+        <NextLink href="/shop" passHref>
+          <HStack>
+            <Icon as={FaStore} boxSize={48}  color={'#002c8b'}/>
+          </HStack>
+        </NextLink>
+        <NextLink href="/about" passHref>
+          <HStack>
+            <Icon as={FaInfoCircle} boxSize={48}  color={'#002c8b'}/>
+          </HStack>
+        </NextLink>
+        <NextLink href="/about" passHref>
+          <HStack>
+            <Icon as={FaInfoCircle} boxSize={48}  color={'#002c8b'}/>
+          </HStack>
+        </NextLink>
+        <NextLink href="/about" passHref>
+          <HStack>
+            <Icon as={FaInfoCircle} boxSize={48} color={'#002c8b'}/>
+          </HStack>
+        </NextLink>
+      </HStack>
+    </Box>
+  );
+}
+
+function Header() {
+  return (
+    <NextLink href="/" passHref style={{ textDecoration: 'none' }}>
+    <Box textAlign="center" p={4} backgroundColor={'#eeeeee'} borderRadius={'16px'} cursor="pointer">
+      <Heading fontFamily="Impact, sans-serif">
+        <Text as="span" color="#ad0000" fontSize={'64px'}>
+          JAM
+        </Text>
+        <Text as="span" color="#002c8b" fontSize={'64px'}>
+          TASTIC
+        </Text>
+      </Heading>
+      <Heading
+        fontSize={'24px'}
+        color="#002c8b"
+        mt={0}
+        fontFamily="Quicksand, sans-serif"
+        fontWeight="normal"
+      >
+        ORGANIC JAM
+      </Heading>
+    </Box>
+    </NextLink>
+  );
+}
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <Box minHeight="100vh" position="relative" pb={20}>
+      <Header />
+      <Box p={4}>{children}</Box>
+      <Menu />
+    </Box>
   );
 }
---
+++ src/pages/shop.tsx
@@ -0,0 +1,86 @@
+import {
+  Box,
+  Heading,
+  SimpleGrid,
+  Text,
+  Image,
+  VStack,
+  Button,
+} from "@chakra-ui/react";
+import products from "../data/products.json";
+import Layout from "../app/layout";
+
+export default function Shop() {
+  return (
+    <Layout>
+      <Box bg="white" minHeight="100vh" mb={60}>
+        <Box
+          textAlign="center"
+          backgroundColor={"#eeeeee"}
+          borderRadius={"16px"}
+        >
+          <Heading textAlign="center" fontFamily="Impact, sans-serif">
+            <Text as="span" color="#ad0000" fontSize={"36px"}>
+              SHOP
+            </Text>
+          </Heading>
+        </Box>
+
+        {/* Products Grid */}
+        <SimpleGrid columns={2} spacing="8px">
+          {products.map((product) => (
+            <Box
+              key={product.id}
+              borderRadius="16px"
+              p={0}
+              pb={2}
+              mb={4}
+              boxShadow="lg"
+              textAlign="center"
+              backgroundColor={"#eeeeee"}
+            >
+              <VStack mb={16} spacing={0}>
+                <Image
+                  src={"/images/" + product.images[0]}
+                  alt={product.name}
+                  borderRadius="16px"
+                  w="100%"
+                  maxHeight="200px"
+                  objectFit="cover"
+                  aspectRatio={1}
+                />
+                <Text
+                  fontFamily="'Bebas Neue', sans-serif"
+                  fontWeight="bold"
+                  color="#ad0000"
+                  fontSize={"18px"}
+                >
+                  {product.name.toUpperCase()}
+                </Text>
+                <Text>Size: {product.size}</Text>
+                <Text
+                  fontFamily="'Bebas Neue', sans-serif"
+                  color="#ad0000"
+                  fontSize={"28px"}
+                  fontWeight={"bold"}
+                >
+                  {product.price.toFixed(2)} €
+                </Text>
+                <Button
+                  bg="#002c8b"
+                  color="white"
+                  _hover={{ bg: "#001d6b" }}
+                  variant="solid"
+                  size={"sm"}
+                  colorScheme="teal"
+                >
+                  ADD
+                </Button>
+              </VStack>
+            </Box>
+          ))}
+        </SimpleGrid>
+      </Box>
+    </Layout>
+  );
+}
