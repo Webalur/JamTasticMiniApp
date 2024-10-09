525b9b5c4895f8fdf3f557b004bbc2db4ae706d7
Aleksey Petrenko <>
2024-10-09 18:28:41 +0200

    WAU-0001 create content

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-fbb9828f7d070944.js","static/chunks/fd9d1056-7eae19b39069738a.js","static/chunks/117-986ff31acdd8d609.js","static/chunks/main-app-a5ec32b22f9c7a57.js"],pages:{"/_app":["static/chunks/webpack-fbb9828f7d070944.js","static/chunks/framework-00a8ba1a63cfdc9e.js","static/chunks/main-a7f8f50291b7f4a7.js","static/chunks/pages/_app-15e2daefa259f0b5.js"],"/_error":["static/chunks/webpack-fbb9828f7d070944.js","static/chunks/framework-00a8ba1a63cfdc9e.js","static/chunks/main-a7f8f50291b7f4a7.js","static/chunks/pages/_error-28b803cb2479b966.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
